1a50657bab23f262340d3f37fafd948cef000b03
Bleckfi <pashkovskiy.serge@gmail.com>
2024-06-17 15:38:34 +0300

    move

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Catalog from "./Components/Catalog";
 import VideoWelcome from "./Components/welcomeVideo";
-import { getStatus } from "./layout";
+import axios from "axios";
 export default async function Home() {
   const nowCame = await getStatus("status", "ongoing", "anime_status");
   const came = await getStatus("status", "released", "anime_status");
@@ -17,3 +17,17 @@ export default async function Home() {
     </>
   );
 }
+
+export const getStatus = async (api, status, anime_status) => {
+  try {
+    const res = await axios.get(
+      `http://localhost:3000/api/${api}?material_data.${anime_status}=${status}`
+    );
+    return {
+      anime: res.data,
+    };
+  } catch (error) {
+    console.error("Ошибка при получении данных:", error);
+    return null;
+  }
+};
